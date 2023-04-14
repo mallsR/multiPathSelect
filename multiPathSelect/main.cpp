940301546ab16cf21698c6d5f065e4cc517971c9
mallsR <2471385107@qq.com>
2023-04-14 17:30:50 +0800

    Initial Commit

---
+++ multiPathSelect/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  multiPathSelect
+//
+//  Created by xiaoR on 2023/4/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
